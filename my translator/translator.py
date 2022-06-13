5a1de85f02353ddb223e44c9817e7f327f613574
LuannSP <luann2v2v@hotmail.com>
2022-06-13 18:43:35 -0300

    translator.py add

---
+++ my translator/translator.py
@@ -0,0 +1,21 @@
+from googletrans import Translator
+
+
+translator = Translator()
+
+
+def translate_text(text, dest='en'):
+    if text and dest != "":
+        return translator.translate(text, dest=dest).text
+    raise ValueError("text empty")
+
+
+def translate_multi_texts2(*args, dest='en'):
+    args = translator.translate(args, dest=dest)
+    return args.text
+
+
+def detect_language(text):
+    if (translator.detect(text).confidence) < 0.7:
+        raise Exception("confidence is low")
+    return translator.detect(text).lang
