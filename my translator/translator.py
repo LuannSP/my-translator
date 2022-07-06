f79d63c9f9a30ecff29cdb4d7db40ffa79780b4b
LuannSP <luann2v2v@hotmail.com>
2022-07-06 18:18:15 -0300

    type hints

--- my translator/translator.py
+++ my translator/translator.py
@@ -1,21 +1,22 @@
 from googletrans import Translator
-
+from typing import Any, List
 
 translator = Translator()
 
-
-def translate_text(text, dest='en'):
-    if text and dest != "":
-        return translator.translate(text, dest=dest).text
+def translate_text(elem: str, dest: str = "en") -> Any:
+    if type(elem) != str:
+        return elem
+    if elem and dest != "":
+        return translator.translate(elem, dest=dest).text
     raise ValueError("text empty")
 
 
-def translate_multi_texts(dest='en', *args):
+def translate_multi_texts(dest: str = "en", *args: str) -> List:
     args = translator.translate(args, dest=dest)
     return args.text
 
 
-def detect_language(text):
-    if (translator.detect(text).confidence) < 0.7:
+def detect_language(elem: str) -> (Any | str):
+    if (translator.detect(elem).confidence) < 0.7:
         raise Exception("confidence is low")
-    return translator.detect(text).lang
+    return translator.detect(elem).lang
